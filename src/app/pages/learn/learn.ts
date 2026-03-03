f41500a8e854309a43dc78402b16a10648e10e09
Danika <dazcha@hotmail.com>
2026-03-03 10:31:03 +0000

    switched to primeng, added new dropdowns, added poppins font

--- src/app/pages/learn/learn.ts
+++ src/app/pages/learn/learn.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
-import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+
+interface Dropdown {
+  code: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-learn',
-  imports: [MatSelectModule],
+  imports: [FormsModule, SelectModule],
   templateUrl: './learn.html',
   styleUrl: './learn.scss',
 })
 export class Learn {
-  frameworks = [{ value: 'flutter', viewValue: 'Flutter' }];
-  categories = [
-    { value: 'requirements', viewValue: 'Requirements' },
-    { value: 'commands', viewValue: 'Commands' },
-    { value: 'functions', viewValue: 'Functions' },
-    { value: 'variables', viewValue: 'Variables' },
+  frameworks: Dropdown[] = [{ code: 'flutter', name: 'Flutter' }];
+  selectedFramework: Dropdown | undefined;
+
+  categories: Dropdown[] = [
+    { code: 'requirements', name: 'Requirements' },
+    { code: 'commands', name: 'Commands' },
+    { code: 'functions', name: 'Functions' },
+    { code: 'variables', name: 'Variables' },
   ];
+  selectedCategory: Dropdown | undefined;
 }
